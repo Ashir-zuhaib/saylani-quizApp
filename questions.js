8269257f0ffb3df4eaf954cd7107e3f2a80fc2a7
Ashir-zuhaib <aashir2027270@gmail.com>
2020-09-14 11:56:02 +0500

    quiz app is ready to commit

---
+++ questions.js
@@ -0,0 +1,29 @@
+var questions = [{
+    "question": "Q. The first mechanical computer designed by charles Babbage was called ?",
+    "option1": "Abacus",
+    "option2": "Calculator",
+    "oprion3": "processor",
+    "option4": "Analytical Engine",
+    "answer":   "4"
+},{
+    "question": "Q. who was the first prime minister of pakistan ?",
+    "option1": "QUAID E AZAM",
+    "option2": "ASHIR ZUHAIB",
+    "oprion3": "LIAQAT ALI KHAN",
+    "option4": "NAWAZ SHARIF",
+    "answer":   "3"
+},{
+    "question": "Q. who is father of computer ?",
+    "option1": "MARK ZUCKERBERG",
+    "option2": "CHARLES BABBAGE",
+    "oprion3": "IMRAN KHAN",
+    "option4": "AMIR KHAN",
+    "answer":   "2"
+},{
+    "question": "Q. Keyboard is a/an ________  ?",
+    "option1": "operating system",
+    "option2": "output device",
+    "oprion3": "Inout device",
+    "option4": "processor",
+    "answer":   "3"
+}]
